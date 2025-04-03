13fffcf2d335fc8b08d30434fcccfa749bff90e2
Sean Murphy <sean@iamseanmurphy.com>
2025-04-03 13:20:18 +0000

    3.1.6

--- smaily-for-wp/vendor/composer/installed.php
+++ smaily-for-wp/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'smaily/smaily_for_wp',
-        'pretty_version' => '3.1.5',
-        'version' => '3.1.5.0',
-        'reference' => '0e4797091dc28f1820c3fe8f5184e6c3f7c0985d',
+        'pretty_version' => '3.1.6',
+        'version' => '3.1.6.0',
+        'reference' => '071e2889ffc3ff4a8b18a26851da8edaeab3ce05',
         'type' => 'plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'smaily/smaily_for_wp' => array(
-            'pretty_version' => '3.1.5',
-            'version' => '3.1.5.0',
-            'reference' => '0e4797091dc28f1820c3fe8f5184e6c3f7c0985d',
+            'pretty_version' => '3.1.6',
+            'version' => '3.1.6.0',
+            'reference' => '071e2889ffc3ff4a8b18a26851da8edaeab3ce05',
             'type' => 'plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
